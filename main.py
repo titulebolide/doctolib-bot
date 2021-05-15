24388617498841919890be79ff589b17de8f73b5
titulebolide <seneztit@gmail.com>
2021-05-15 21:25:47 +0200

    fix msg

--- main.py
+++ main.py
@@ -2,28 +2,21 @@ import requests
 import re
 import conf
 
-def get_ids(max_page = 1):
-    ids = []
-    for page in range(1, max_page+1):
-        s = requests.get(
-        "https://www.doctolib.fr/vaccination-covid-19/{}?force_max_limit=2&ref_visit_motive_ids[]=6970&ref_visit_motive_ids[]=7005&page={}".format(conf.LOCATION,page)
-        )
-        ids_raw = re.findall(r'id="search-result-\d+', s.text)
-        ids.extend([int(id.replace('id="search-result-', "")) for id in ids_raw])
-    return ids
+ids = []
+for page in range(1, conf.MAX_PAGE+1):
+    s = requests.get(
+    "https://www.doctolib.fr/vaccination-covid-19/{}?force_max_limit=2&ref_visit_motive_ids[]=6970&ref_visit_motive_ids[]=7005&page={}".format(conf.LOCATION,page)
+    )
+    ids_raw = re.findall(r'id="search-result-\d+', s.text)
+    ids.extend([int(id.replace('id="search-result-', "")) for id in ids_raw])
 
-
-def is_id_available(id):
+for id in ids:
     data = requests.get(
         "https://www.doctolib.fr/search_results/{}.json?force_max_limit=2&ref_visit_motive_ids[]=6970&ref_visit_motive_ids[]=7005".format(id)
     ).json()
     print(data['search_result']['name_with_title'])
-    return data['total'] > 0
 
-ids = get_ids(conf.MAX_PAGE)
-
-for id in ids:
-    if is_id_available(id):
+    if data['total'] > 0:
         msg = """
 ### Créneau vaccin trouvé 💉 !
 Vaccin covid trouvé à [**{}**](https://www.doctolib.fr{})
